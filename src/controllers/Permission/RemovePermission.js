342cddf5f2f8e1f75149c9be27bb83b5ad517fa0
PortalHUN <giotomi03@gmail.com>
2023-03-25 17:36:19 +0100

    Completed RemovePErmission.js

--- src/controllers/Permission/RemovePermission.js
+++ src/controllers/Permission/RemovePermission.js
@@ -1,6 +1,33 @@
 const db = require("../../utils/db");
-const PermissionList = require("./PermissionList");
 
-const RemovePermission = (req, res) => {};
+const RemovePermission = async (req, res) => {
+  const { ID, permissions } = req.body;
+  if (!ID || !permissions || permissions.length == 0) return res.status(403).json({ err: "Invalid data" });
+
+  await db.query(`SELECT Perm FROM permissions WHERE AppID = ${db.escape(ID)};`, async (error, userP) => {
+    if (error) return res.status(500).json({ err: "Database error: " + error.sqlMessage });
+
+    let userPermission = [];
+    userP.forEach((element) => {
+      userPermission.push(element.Perm);
+    });
+
+    let correct = [];
+    permissions.forEach(async (element) => {
+      if (userPermission.includes(element)) correct.push(element);
+    });
+
+    correct.forEach(async (element) => {
+      await db.query(
+        `DELETE FROM permissions WHERE AppID = ${db.escape(ID)} AND Perm = ${db.escape(element)}`,
+        (error, result) => {
+          if (error) return res.status(500).json({ err: "Database error: " + error.sqlMessage });
+        }
+      );
+    });
+
+    return res.status(200).json({ message: "Successfully removed permissions", removed: correct });
+  });
+};
 
 module.exports = RemovePermission;
--- src/routes/PermissionRoutes.js
+++ src/routes/PermissionRoutes.js
@@ -2,4 +2,6 @@ const router = require("express").Router();
 
 router.post("/add", require("../controllers/Permission/AddPermission"));
 
+router.post("/remove", require("../controllers/Permission/RemovePermission"));
+
 module.exports = router;
