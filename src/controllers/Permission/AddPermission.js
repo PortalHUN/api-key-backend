f3cb519ff44de27378e13c3825323b929e0ddc44
PortalHUN <giotomi03@gmail.com>
2023-03-25 17:19:55 +0100

    Fixed AddPermission.js req.api.permissions check

--- src/controllers/Permission/AddPermission.js
+++ src/controllers/Permission/AddPermission.js
@@ -8,30 +8,38 @@ const AddPermission = async (req, res) => {
 
   //Is there correct information we can use?
   //Check if user already has these permissions or if they are valid
-  let correct = [];
-  permissions.forEach((element) => {
-    if (PermissionList.includes(element) && !req.api.permissions.includes(element)) correct.push(element);
-  });
-
-  if (correct.length == 0) return res.status(400).json({ err: "This user already has these permissions or they are invalid" });
-
-  //User exists?
-  await db.query(`SELECT ID FROM api_keys WHERE ID = ${db.escape(ID)};`, async (error, app) => {
+  await db.query(`SELECT Perm FROM permissions WHERE AppID = ${db.escape(ID)};`, async (error, perm) => {
     if (error) return res.status(500).json({ err: "Database error: " + error.sqlMessage });
-    else if (app.lenght == 0) return res.status(400).json({ err: "No App found with this ID" });
+    let permission = [];
+    perm.forEach((element) => {
+      permission.push(element.Perm);
+    });
 
-    //Putting together the sql string
-    let sqlString = `INSERT INTO permissions(AppID, Perm) VALUES `;
-    for (let i = 0; i < correct.length; i++) {
-      const element = correct[i];
-      if (i == correct.length - 1) sqlString += `(${db.escape(ID)}, ${db.escape(element)});`;
-      else sqlString += `(${db.escape(ID)}, ${db.escape(element)}), `;
-    }
+    let correct = [];
+    permissions.forEach(async (element) => {
+      if (PermissionList.includes(element) && !permission.includes(element)) correct.push(element);
+    });
 
-    //Inserting rows
-    await db.query(sqlString, (error, result) => {
+    if (correct.length == 0) return res.status(400).json({ err: "This user already has these permissions or they are invalid" });
+
+    //User exists?
+    await db.query(`SELECT ID FROM api_keys WHERE ID = ${db.escape(ID)};`, async (error, app) => {
       if (error) return res.status(500).json({ err: "Database error: " + error.sqlMessage });
-      else return res.status(200).json({ message: "Successfuly added permissions", added: correct });
+      else if (app.lenght == 0) return res.status(400).json({ err: "No App found with this ID" });
+
+      //Putting together the sql string
+      let sqlString = `INSERT INTO permissions(AppID, Perm) VALUES `;
+      for (let i = 0; i < correct.length; i++) {
+        const element = correct[i];
+        if (i == correct.length - 1) sqlString += `(${db.escape(ID)}, ${db.escape(element)});`;
+        else sqlString += `(${db.escape(ID)}, ${db.escape(element)}), `;
+      }
+
+      //Inserting rows
+      await db.query(sqlString, (error, result) => {
+        if (error) return res.status(500).json({ err: "Database error: " + error.sqlMessage });
+        else return res.status(200).json({ message: "Successfuly added permissions", added: correct });
+      });
     });
   });
 };
---
+++ src/controllers/Permission/RemovePermission.js
@@ -0,0 +1,6 @@
+const db = require("../../utils/db");
+const PermissionList = require("./PermissionList");
+
+const RemovePermission = (req, res) => {};
+
+module.exports = RemovePermission;
